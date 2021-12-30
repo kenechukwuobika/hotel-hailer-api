1f3038b800f770dbf70f538e179f38eab2a7292c
obikakelvin <obikakelvin.2255@gmail.com>
2021-12-30 17:43:02 +0100

    minor updates

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -15,7 +15,6 @@ module.exports = class Email{
             return nodemailer.createTransport({
                 host: process.env.EMAIL_HOST,
                 port: process.env.EMAIL_PORT,
-                secure: true,
                 auth: {
                   user: process.env.EMAIL_USERNAME,
                   pass: process.env.EMAIL_PASSWORD
