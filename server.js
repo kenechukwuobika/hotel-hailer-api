2626b01f42e68f4f7b09931510c669a2ac624d4a
obikakelvin <obikakelvin.2255@gmail.com>
2022-01-06 14:26:05 +0100

    switched node env to production

--- server.js
+++ server.js
@@ -30,7 +30,5 @@ mongoose.connect(db, {
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(process.env.NODE_ENV)
-    process.env.NODE_ENV='production'
-    console.log(process.env.NODE_ENV)
     console.log(`listening to server on port ${process.env.PORT}`);
 })
