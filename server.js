c04261a53bf005a33ab2846e3f66d29a7bda2a09
obikakelvin <obikakelvin.2255@gmail.com>
2022-01-06 14:29:33 +0100

    switched node env to development

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ mongoose.connect(db, {
 });
 
 app.listen(process.env.PORT || 3000, () => {
+    process.env.NODE_ENV='development'
     console.log(process.env.NODE_ENV)
     console.log(`listening to server on port ${process.env.PORT}`);
 })
