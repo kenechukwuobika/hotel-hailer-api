e5cd601fe1bcad66923e27fae2cac06ff3cd9c4e
obikakelvin <obikakelvin.2255@gmail.com>
2022-04-01 09:48:28 +0100

    revamped booking system

--- routes/bookingRoute.js
+++ routes/bookingRoute.js
@@ -8,8 +8,8 @@ const router = express.Router();
 
 router.use(authController.protect)
 router.post('/transationWebhook', bookingController.transactionWebhook);
-router.post('/getBookedDates/:propertyId', bookingController.getBookedDates);
-router.post('/checkPlanEligibility/:propertyId', bookingController.checkPlanEligibility);
+router.post('/booked-dates/:propertyId', bookingController.getBookedDates);
+router.post('/booking-plans/:propertyId', bookingController.checkPlanEligibility);
 router.post('/initialize/:propertyId', bookingController.initializeTransaction);
 router.post('/verify', bookingController.verifyTransation);
 
