7595be378dcaa6613655f7f9d5f00fedbe6ec8a0
obikakelvin <obikakelvin.2255@gmail.com>
2022-05-11 17:07:13 +0100

    minor fix

--- utilities/catchAsync.js
+++ utilities/catchAsync.js
@@ -1,1 +1,9 @@
-module.exports = fn => (req, res, next) => fn(req, res, next).catch(next);
+const AppException = require('./AppException');
+
+module.exports = fn => (req, res, next) => {
+    if(typeof(fn) !== 'function'){
+        console.log(fn)
+        return next(new AppException(400, 'fn is spposed to be a function'))
+    }
+    return fn(req, res, next).catch(next)
+};
